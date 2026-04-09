9055ba7255ab6331924a36a8ee48eedddf5ac3ee
FireChat♂ <fire.chat.27@gmail.com>
2026-04-10 01:04:48 +0200

    Various small fixes

--- games/drum_live/drum_live_menu_engine.c
+++ games/drum_live/drum_live_menu_engine.c
@@ -48,7 +48,7 @@ const char *drum_live_menu_poster_desc[] = {
     "\n",
 
     /* DRUM BAND  */
-    "\x03\x31" "\x01\x6d" "\x05\x30" "\x01\x34" "Samouraˆ´ Tom       et sa bande \n"
+    "\x03\x31" "\x01\x6d" "\x05\x30" "\x01\x34" "Samouraˆ´ Tom\n  et sa bande\n"
     "\x03\x31" "\x01\x6d" "\x05\x30" "\x01\x34" "\x01\x52" "en CONCERT ! \n"
     "\x03\x30" "\x01\x73" "\x05\x31" "\x01\x35" "\x01\x4c" "ô@Danse cosmique\n"
     "\x03\x30" "\x01\x73" "\x05\x31" "\x01\x35" "ô@Trio western clap\n"
